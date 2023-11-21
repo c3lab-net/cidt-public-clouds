86f4b7a4e5ee96891d8e69b3873e65ced98d54d0
Yibo Guo <nil.yibo@gmail.com>
2023-11-21 10:15:37 -0800

    Output to route distribution to TSV.

--- analysis/distribution.routes.py
+++ analysis/distribution.routes.py
@@ -7,23 +7,28 @@ import logging
 import sys
 from typing import Any, Optional
 
+import pandas as pd
+
 from common import RouteMetric, calculate_route_metric, get_routes_from_file, init_logging
 
 def export_routes_distribution(routes: list[list], metrics:list[RouteMetric],
                                output: Optional[io.TextIOWrapper] = None):
     logging.info('Exporting routes distribution ...')
 
-    # Header
-    print('count', *metrics, 'route', file=output if output else sys.stdout)
+    columns = ['count'] + [metric for metric in metrics] + ['route']
 
     routes_as_str = [ '|'.join([str(e) for e in route]) for route in routes ]
+    rows = []
     for route_str, count in sorted(Counter(routes_as_str).items(), key=lambda x: x[1], reverse=True):
         row: list[Any] = [count]
         for metric in metrics:
             value = calculate_route_metric(route_str, metric)
             row.append(value)
         row.append(route_str)
-        print(*row, file=output if output else sys.stdout)
+        rows.append(row)
+
+    df = pd.DataFrame(rows, columns=columns)
+    df.to_csv(output if output else sys.stdout, sep='\t', index=False)
 
     logging.info('Done')
 
@@ -34,7 +39,7 @@ def parse_args():
                         help='Export the routes distribution.')
     parser.add_argument('--include', nargs='*', type=RouteMetric, choices=list(RouteMetric),
                         help='The additional metrics to include.')
-    parser.add_argument('-o', '--output', type=argparse.FileType('w'), help='The output file.')
+    parser.add_argument('-o', '--output-tsv', type=argparse.FileType('w'), help='The output TSV file.')
     args = parser.parse_args()
 
     if not args.include:
@@ -48,7 +53,7 @@ def main():
 
     if args.export_routes_distribution:
         routes = get_routes_from_file(args.routes_file)
-        export_routes_distribution(routes, args.include, args.output)
+        export_routes_distribution(routes, args.include, args.output_tsv)
     else:
         raise ValueError('No action specified')
 
