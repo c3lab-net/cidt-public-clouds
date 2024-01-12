d49f1a9edb114aef9c162a0892c5d2f8594d1cf6
Yibo Guo <nil.yibo@gmail.com>
2024-01-11 16:01:29 -0800

    Supports loading from TSV and per-region-pair distribution files.

--- analysis/plot.routes.all_region_pairs.py
+++ analysis/plot.routes.all_region_pairs.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 
 import argparse
-import ast
 import csv
+import io
 import logging
 import os
 import re
-from matplotlib.colors import LinearSegmentedColormap, Normalize
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from typing import Any, Callable, Optional
+from typing import Any, Optional
 
 from common import DirType, RouteMetric, calculate_route_metric, init_logging
 
@@ -25,24 +24,24 @@ def load_weighted_hops(file_path: str) -> pd.DataFrame:
         1 hop1|hop2
         ...
     """
-    l_weight = []
-    l_hops = []
+    l_weight: list[float] = []
+    l_hops: list[str] = []
 
     with open(file_path, 'r') as file:
         reader = csv.DictReader(file, delimiter='\t')
         for row in reader:
             weight = float(row['count'])
-            hops = row['route']
+            hops: str = row['route']
             l_weight.append(weight)
             l_hops.append(hops)
 
     # Create a DataFrame
     return pd.DataFrame.from_dict({'weight': l_weight, 'hops': l_hops})
 
-def get_hops_and_weights(file_path: str) -> tuple[np.ndarray, np.ndarray]:
+def get_hops_and_weights(file_path: str) -> tuple[list[str], list[float]]:
     data = load_weighted_hops(file_path)
-    hops = data['hops'].to_numpy()
-    weights = data['weight'].to_numpy()
+    hops: list[str] = data['hops'].to_list()
+    weights: list[float] = data['weight'].to_list()
     return hops, weights
 
 def extract_cloud_regions_from_filename(filename) -> tuple[tuple[str, str], tuple[str, str]]:
@@ -73,8 +72,8 @@ def plot_single_pair_pdf(values, weights, src_region: str, dst_region: str, metr
     logging.info(f'Saving heatmap to {filename} ...')
     plt.savefig(filename, bbox_inches='tight')
 
-def plot_heatmap(src_regions: list[str], dst_regions: list[str], region_hop_counts: dict[tuple[str, str], float],
-                 metric: str, data_source: str):
+def plot_heatmap(src_regions: list[str], dst_regions: list[str], metric_value_by_region: dict[tuple[str, str], float],
+                 metric: RouteMetric, data_source: str):
     # Create a mapping of regions to indices
     src_region_to_index = {region: index for index, region in enumerate(src_regions)}
     dst_region_to_index = {region: index for index, region in enumerate(dst_regions)}
@@ -83,7 +82,7 @@ def plot_heatmap(src_regions: list[str], dst_regions: list[str], region_hop_coun
     matrix = np.zeros((len(src_regions), len(dst_regions)))
 
     # Populate the matrix
-    for (src_region, dst_region), value in region_hop_counts.items():
+    for (src_region, dst_region), value in metric_value_by_region.items():
         i, j = src_region_to_index[src_region], dst_region_to_index[dst_region]
         matrix[i][j] = value
 
@@ -101,12 +100,12 @@ def plot_heatmap(src_regions: list[str], dst_regions: list[str], region_hop_coun
     logging.info(f'Saving heatmap to {filename} ...')
     plt.savefig(filename, bbox_inches='tight')
 
-def get_weighted_average_by_region_pair(dirpath: str, process_hops: Callable[[list[str]], Any],
-                                        metric: str, plot_individual_pdfs: bool,
+def get_values_and_weights_by_region_pair_from_raw_files(dirpath: str, metric: RouteMetric,
                                         src_cloud: Optional[str], src_region: Optional[str],
-                                        dst_cloud: Optional[str], dst_region: Optional[str]):
+                                        dst_cloud: Optional[str], dst_region: Optional[str]) -> \
+                                            dict[tuple[str, str], tuple[list[Any], list[float]]]:
     # Dictionary to hold the source-destination pairs and the aggregated value
-    weighted_average_by_region_pair = {}
+    values_and_weights_by_region_pair = {}
 
     # Corrected processing of each file
     for file in os.listdir(dirpath):
@@ -121,27 +120,67 @@ def get_weighted_average_by_region_pair(dirpath: str, process_hops: Callable[[li
 
         # Calculate the weighted hop count for the file
         hops, weights = get_hops_and_weights(file_path)
-        values = np.array(list(map(process_hops, hops)))
-
-        # Skip bad values
-        for i in range(values.size):
-            # Ignore the distance if it is 0
-            if values[i] == 0.:
-                weights[i] = 0.
+        values = list(map(lambda x: calculate_route_metric(x, metric), hops))
 
         src = ':'.join(src)
         dst = ':'.join(dst)
-        weighted_average_by_region_pair[(src, dst)] = np.average(values, weights=weights).tolist()
+        values_and_weights_by_region_pair[(src, dst)] = (values, weights)
+    return values_and_weights_by_region_pair
 
-        if plot_individual_pdfs:
-            plot_single_pair_pdf(values, weights, src, dst, metric, DATA_SOURCE)
+def get_values_and_weights_by_region_pair_from_tsv(routes_distribution_tsv: io.TextIOWrapper, metric: RouteMetric,
+                                                   src_cloud: Optional[str], src_region: Optional[str],
+                                                   dst_cloud: Optional[str], dst_region: Optional[str]) -> \
+                                                    dict[tuple[str, str], tuple[list[Any], list[float]]]:
+    # Dictionary to hold the source-destination pairs and the aggregated value
+    values_and_weights_by_region_pair = {}
+    reader = csv.DictReader(routes_distribution_tsv, delimiter='\t')
+    assert reader.fieldnames and metric in reader.fieldnames, f'Field {metric} not found in TSV file'
+    for column in ['src_cloud', 'src_region', 'dst_cloud', 'dst_region']:
+        assert reader.fieldnames and column in reader.fieldnames, f'Field {column} not found in TSV file'
+
+    def _parse_value(value: str, metric: RouteMetric) -> Any:
+        if metric == RouteMetric.HopCount:
+            return int(value)
+        elif metric == RouteMetric.DistanceKM:
+            return float(value)
+        else:
+            return value
+
+    for row in reader:
+        src = (row["src_cloud"], row["src_region"])
+        dst = (row["dst_cloud"], row["dst_region"])
+        if (src_cloud and src_cloud != src[0]) or (src_region and src_region != src[1]) or \
+                (dst_cloud and dst_cloud != dst[0]) or (dst_region and dst_region != dst[1]):
+            continue
+        src = ':'.join(src)
+        dst = ':'.join(dst)
+        key = (src, dst)
+        if key not in values_and_weights_by_region_pair:
+            values_and_weights_by_region_pair[key] = ([], [])
+        values_and_weights_by_region_pair[key][0].append(_parse_value(row[metric], metric))
+        values_and_weights_by_region_pair[key][1].append(float(row['count']))
+    return values_and_weights_by_region_pair
+
+def get_weighted_average_by_region_pair(
+        values_and_weights_by_region_pair: dict[tuple[str, str], tuple[list[Any], list[float]]],
+        ignore_zeros: bool = False):
+    weighted_average_by_region_pair = {}
+    for region_pair, (values, weights) in values_and_weights_by_region_pair.items():
+        values = np.array(values)
+        weights = np.array(weights)
+        if ignore_zeros:
+            values = values[values != 0.]
+            weights = weights[values != 0.]
+        weighted_average_by_region_pair[region_pair] = np.average(values, weights=weights)
     return weighted_average_by_region_pair
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--metrics', required=True, nargs='+', type=RouteMetric, choices=list(RouteMetric),
                         help='The metrics to plot')
-    parser.add_argument('--dirpath', type=DirType, required=True, help='The directory that contains the routes files')
+    parser.add_argument('--dirpath', type=DirType, help='The directory that contains the routes files')
+    parser.add_argument('--routes-distribution-tsv', type=argparse.FileType('r'),
+                        help='The TSV file that contains the routes by region pair')
     parser.add_argument('--plot-heatmap', action='store_true',
                         help='Plot the heatmap of the metric across all region pairs')
     parser.add_argument('--plot-individual-pdfs', action='store_true',
@@ -154,6 +193,9 @@ def parse_args():
 
     if not (args.plot_heatmap or args.plot_individual_pdfs):
         parser.error('At least one of --plot-heatmap or --plot-individual-pdfs must be specified')
+
+    if args.dirpath is not None != args.routes_distribution_tsv is not None:
+        parser.error('Either --dirpath or --routes-distribution-tsv must be specified')
 
     if args.src_region and not args.src_cloud:
         parser.error('--src-cloud must be specified with --src-region')
@@ -168,16 +210,27 @@ def main():
     args = parse_args()
 
     for metric in args.metrics:
-        process_hops = lambda x: calculate_route_metric(x, metric)
-        value_by_region_pair = get_weighted_average_by_region_pair(args.dirpath, process_hops, metric,
-                                                                   args.plot_individual_pdfs,
-                                                                   args.src_cloud, args.src_region,
-                                                                   args.dst_cloud, args.dst_region)
-        src_dst_pairs = value_by_region_pair.keys()
-        src_regions = sorted(set(t[0] for t in src_dst_pairs))
-        dst_regions = sorted(set(t[1] for t in src_dst_pairs))
+        if args.dirpath:
+            values_and_weights_by_region_pair = get_values_and_weights_by_region_pair_from_raw_files(
+                args.dirpath, metric, args.src_cloud, args.src_region, args.dst_cloud, args.dst_region)
+        elif args.routes_distribution_tsv:
+            values_and_weights_by_region_pair = get_values_and_weights_by_region_pair_from_tsv(
+                args.routes_distribution_tsv, metric, args.src_cloud, args.src_region, args.dst_cloud, args.dst_region)
+        else:
+            raise ValueError('Either --dirpath or --routes-distribution-tsv must be specified')
+
+        if args.plot_individual_pdfs:
+            for (src, dst), (values, weights) in values_and_weights_by_region_pair.items():
+                plot_single_pair_pdf(values, weights, src, dst, metric, DATA_SOURCE)
+
         if args.plot_heatmap:
-            plot_heatmap(src_regions, dst_regions, value_by_region_pair, metric, DATA_SOURCE)
+            ignore_zeros = metric in (RouteMetric.HopCount, RouteMetric.DistanceKM)
+            weighted_average_by_region_pair = get_weighted_average_by_region_pair(
+                values_and_weights_by_region_pair, ignore_zeros)
+            region_pairs = weighted_average_by_region_pair.keys()
+            src_regions = sorted(set(t[0] for t in region_pairs))
+            dst_regions = sorted(set(t[1] for t in region_pairs))
+            plot_heatmap(src_regions, dst_regions, weighted_average_by_region_pair, metric, DATA_SOURCE)
 
 if __name__ == '__main__':
     main()
